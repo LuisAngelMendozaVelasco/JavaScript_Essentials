6e4ab2aba270be3e7b20350916cfce63b78bd1e4
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-12-03 12:39:24 -0600

    Create Example7.js

---
+++ OperatorsAndUserInteraction/Example7.js
@@ -0,0 +1,18 @@
+/*
+"Compound Assignment Operators"
+
+Just like arithmetic operators, binary logical operators can be used in combination with the assignment operator, creating a logical AND assignment &&= and a logical 
+OR assignment ||=.
+*/
+
+// The instruction a &&= false means exactly the same as a = a && false.
+let a = true;
+console.log(a); // -> true
+a &&= false;
+console.log(a); // -> false
+
+// This time, the operation b ||= true is interpreted as b = b || true.
+let b = false;
+console.log(b); // -> false
+b ||= true;
+console.log(b); // -> true
