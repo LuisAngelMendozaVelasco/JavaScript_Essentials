19fd730c739269a5fc9cd1e174e3bbf07dbe8f21
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-12-05 20:19:31 -0600

    Update Example17.js

--- VariablesDataTypesTypeCastingAndComments/Example17.js
+++ VariablesDataTypesTypeCastingAndComments/Example17.js
@@ -85,3 +85,57 @@ continent = "Africa";
 
 let sentence = `${country} is located in ${continent}.`;
 console.log(sentence); // -> Malawi is located in Africa.
+
+/*
+A method is a special kind of function that belongs to an object. Objects are complex data types, which can consist of many values (stored in properties) and methods. 
+If we want to call the method of an object, we write the name of the method after a dot. This is exactly the notation we use when calling console.log. The console 
+object has many other methods besides the log method, such as time and timeEnd (which can be used to measure time).
+*/
+console.time();
+console.log("test console"); // -> test console
+console.timeEnd(); // -> default: 0.108154296875 ms
+
+/*
+All data of primitive types such as Number, BigInt, Boolean, or String have corresponding objects to which they can be converted. Each of these objects will have 
+methods designed for a specific data type. At this point, we come to another concept, that is, autoboxing. If a dot appears after a literal representing a primitive 
+type, or after a variable containing this type of data, the JavaScript interpreter tries to treat this value as an object and not a primitive. For this purpose, 
+it converts the primitive to the corresponding object on the fly, which has the appropriate methods (i.e. it performs autoboxing).
+
+In the variable river, we store the primitive of a String type. In the next line, we refer to this variable, writing a dot after its name and the name of one of the 
+methods – charAt (a method of the String class object). Although the primitive has no methods that can be called, the interpreter temporarily converts this value to 
+a suitable object that already has such methods. One of these methods is charAt, which we now call. The method operates on a string originally placed in the river 
+variable, and returns a single letter from the specified position (letters are counted starting from 0).
+
+After the operation is completed, the interpreter removes the temporary object. So from our point of view, it looks like we just called a method on a given 
+primitive type.
+*/
+let river = "Mekong";
+let character = river.charAt(2);
+console.log(character); // -> k
+
+/*
+Commonly used string methods and properties (i.e. named values related to the object) are:
+
+    length: property, returns the number of characters in a string;
+
+    charAt(index): method, returns the character at the "index" position in the string (indexes start from 0);
+
+    slice(beginIndex, [optional] endIndex): method, returns a new string that is created from the characters between beginIndex (included) and endIndex (excluded); 
+    if endIndex is omitted, then the new string is from beginIndex to the end of the string;
+
+    split(separator, [optional] limit): method, splits the string into substrings whenever a separator is found in that string, and returns an array of those substrings 
+    (we will say a few words about arrays in a moment), while an optional limit limits the number of substrings added to the list.
+*/
+let str = "java script language";
+
+console.log(str.length); // -> 20
+console.log('test'.length); // -> 4
+
+console.log(str.charAt(0)); // -> 'j'
+console.log('abc'.charAt(1)); // -> 'b'
+
+console.log(str.slice(0, 4)); // -> 'java'
+console.log('test'.slice(1, 3)); // -> 'es'
+
+console.log(str.split(' ')); // -> ['java', 'script', 'language']
+console.log('192.168.1.1'.split('.'));  // -> ['192', '168', '1', '1']
