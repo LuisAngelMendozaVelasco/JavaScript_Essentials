d6c0a86cd5efab5b90a09d5fbdac38fa3844f133
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-12-07 08:28:20 -0600

    Update Example9.js

--- OperatorsAndUserInteraction/Example9.js
+++ OperatorsAndUserInteraction/Example9.js
@@ -13,3 +13,9 @@ let newSentence = sentence + 10191;
 
 console.log(newSentence); // -> Happy New Year 10191
 console.log(typeof newSentence); // -> string
+
+// Compound Assignment Operators
+sentence = "Happy New ";
+sentence += "Year ";
+sentence += 10191;
+console.log(sentence); // -> Happy New Year 10191
