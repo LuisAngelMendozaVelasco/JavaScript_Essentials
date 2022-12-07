d880ce46a8e74666c2d817ab7ae78ae5be2929a5
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-12-06 21:20:41 -0600

    Create Example36.js

---
+++ VariablesDataTypesTypeCastingAndComments/Example36.js
@@ -0,0 +1,14 @@
+/*
+"shift"
+
+The shift method works similarly to pop, only this time we remove the element from the beginning of the array (with the index 0). The removed element is returned 
+by the method, and all other elements are shifted to the left, filling the empty space. The length of the original array is reduced by 1.
+*/
+
+let names = ["Olivia", "Emma", "Mateo", "Samuel"];
+console.log(names.length); // -> 4
+
+let name = names.shift();
+console.log(names.length); // -> 3
+console.log(name); // -> Olivia
+console.log(names); // -> ["Emma", "Mateo", "Samuel"]
