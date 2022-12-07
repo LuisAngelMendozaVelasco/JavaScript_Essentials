0e8c447d98deb1a273663f52c0af2c88d2923bc0
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-12-07 08:24:54 -0600

    Update Example8.js

--- OperatorsAndUserInteraction/Example8.js
+++ OperatorsAndUserInteraction/Example8.js
@@ -13,7 +13,7 @@ console.log(2 _ 4);           // expected 16
 console.log(5 _ 1);           // expected 5  
 console.log(8 _ 2 _ 5 _ 2);   // expected 39
 */
-console.log(2 * 3 + 1);       
+console.log(2 * 3 + 1);
 console.log(2 ** 4);
 console.log(5 * 1); 
 console.log(8 ** 2 - 5 ** 2);
