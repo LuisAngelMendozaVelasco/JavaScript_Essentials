c4f22c1a1f9aa6eef666c9b2152a1915db1478d2
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-12-06 20:50:13 -0600

    Create Example31.js

---
+++ VariablesDataTypesTypeCastingAndComments/Example31.js
@@ -0,0 +1,16 @@
+/*
+"length"
+
+The length property is used to get information about the length (the number of elements) of the array (including empty positions between existing elements).
+*/
+
+let names  = ["Olivia", "Emma", "Mateo", "Samuel"];
+console.log(names.length); // -> 4
+
+names[5] = "Amelia";
+console.log(names.length); // -> 6
+
+console.log(names); // -> ["Olivia", "Emma", "Mateo", "Samuel", undefined, "Amelia"]
+console.log(names[3]); // -> Samuel
+console.log(names[4]); // -> undefined
+console.log(names[5]); // -> Amelia
